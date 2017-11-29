2fd8bfbd0735de0e7e7f6cde875e11fb1854d98d
Léo Statkus <leo@statkus.fr>
2017-11-29 14:49:30 +0100

    [DEV] Now trees can grow !

--- SogiTree.py
+++ SogiTree.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import fileinput
+import random
 
 matrix = []
 
@@ -14,14 +15,101 @@ for line in fileinput.input():
 ####################
 # Grow the trees ! #
 ####################
-for l in range(0, len(matrix)):
-    for c in range(0, len(matrix[l])):
-        if matrix[l][c] == 't':
-            matrix[l][c] = 'T'
+for l in range(1, len(matrix)-2):
+    for c in range(1, len(matrix[l])-1):
+        if matrix[l][c] == 'r':
+            # Grow the root
+            # R : new leaf
+            growing_chance = 0.15
+            n_of_new = 0
+            n_of_new_allowed = 1
+            if matrix[l+1][c-1] == '-':
+                if random.random() < growing_chance - 0.1:
+                    matrix[l+1][c-1] = 'R'
+                    n_of_new += 1
+            if matrix[l+1][c] == '-':
+                if random.random() < growing_chance - 0.1:
+                    matrix[l+1][c] = 'R'
+                    n_of_new += 1
+            if matrix[l+1][c+1] == '-' and n_of_new < n_of_new_allowed:
+                if random.random() < growing_chance - 0.1:
+                    matrix[l+1][c+1] = 'R'
+                    n_of_new += 1
+            if matrix[l][c-1] == '-' and n_of_new < n_of_new_allowed:
+                if random.random() < growing_chance - 0.0:
+                    matrix[l][c-1] = 'R'
+                    n_of_new += 1
+            if matrix[l][c+1] == '-' and n_of_new < n_of_new_allowed:
+                if random.random() < growing_chance - 0.0:
+                    matrix[l][c+1] = 'R'
+                    n_of_new += 1
+
+        elif matrix[l][c] == 'T':
+            if random.random() < 0.07:
+                # N : new trunc
+                if matrix[l][c-1] == ' ':
+                    matrix[l][c-1] = 'N'
+                if matrix[l][c+1] == ' ':
+                    matrix[l][c+1] = 'N'
+
+        elif matrix[l][c] == 't':
+            if random.random() < 0.15:
+                matrix[l][c] = 'T'
+
         elif matrix[l][c] == 'F':
-            matrix[l][c] = 't'
+            if random.random() < 0.2:
+                matrix[l][c] = 't'
+
         elif matrix[l][c] == 'f':
-            matrix[l][c] = 'F'
+            if random.random() < 0.3:
+                matrix[l][c] = 'F'
+
+            # Grow the tree around the leaf
+            # n : new leaf
+            growing_chance = 0.3
+            n_of_new = 0
+            n_of_new_allowed = 2
+            if matrix[l-1][c-1] == ' ':
+                if random.random() < growing_chance:
+                    matrix[l-1][c-1] = 'n'
+                    n_of_new += 1
+            if matrix[l-1][c] == ' ':
+                if random.random() < growing_chance:
+                    matrix[l-1][c] = 'n'
+                    n_of_new += 1
+            if matrix[l-1][c+1] == ' ' and n_of_new < n_of_new_allowed:
+                if random.random() < growing_chance:
+                    matrix[l-1][c+1] = 'n'
+                    n_of_new += 1
+            if matrix[l][c-1] == ' ' and n_of_new < n_of_new_allowed:
+                if random.random() < growing_chance - 0.1:
+                    matrix[l][c-1] = 'n'
+                    n_of_new += 1
+            if matrix[l][c+1] == ' ' and n_of_new < n_of_new_allowed:
+                if random.random() < growing_chance - 0.1:
+                    matrix[l][c+1] = 'n'
+                    n_of_new += 1
+            #if matrix[l+1][c-1] == ' ' and n_of_new < n_of_new_allowed:
+            #    if random.random() < growing_chance - 0.2:
+            #        matrix[l+1][c-1] = 'n'
+            #        n_of_new += 1
+            #if matrix[l+1][c] == ' ' and n_of_new < n_of_new_allowed:
+            #    if random.random() < growing_chance - 0.2:
+            #        matrix[l+1][c] = 'n'
+            #        n_of_new += 1
+            #if matrix[l+1][c+1] == ' ' and n_of_new < n_of_new_allowed:
+            #    if random.random() < growing_chance - 0.2:
+            #        matrix[l+1][c+1] = 'n'
+
+# Replace the new leaf (n) by leaf (f) and the new trunc (N) by trunc (T) and the new root "R"
+for l in range(1, len(matrix)-1):
+    for c in range(1, len(matrix[l])-1):
+        if matrix[l][c] == 'n':
+            matrix[l][c] = 'f'
+        elif matrix[l][c] == 'N':
+            matrix[l][c] = 'T'
+        elif matrix[l][c] == 'R':
+            matrix[l][c] = 'r'
 
 #######################
 # Write modified file #
