e31805b7d8ba9c08c7c898fc1d37d52cc726b82d
Léo Statkus <leo@statkus.fr>
2017-11-29 13:37:32 +0100

    [FIX] We do not need to iterate ourselve, make video do the job for us.

--- SogiTree.py
+++ SogiTree.py
@@ -11,18 +11,17 @@ for line in fileinput.input():
     line = line[0:-1]
     matrix.append(list(line))
 
-for i in range(0, 10):
-    ####################
-    # Grow the trees ! #
-    ####################
-    for l in range(0, len(matrix)):
-        for c in range(0, len(matrix[l])):
-            if matrix[l][c] == 't':
-                matrix[l][c] = 'T'
-            elif matrix[l][c] == 'F':
-                matrix[l][c] = 't'
-            elif matrix[l][c] == 'f':
-                matrix[l][c] = 'F'
+####################
+# Grow the trees ! #
+####################
+for l in range(0, len(matrix)):
+    for c in range(0, len(matrix[l])):
+        if matrix[l][c] == 't':
+            matrix[l][c] = 'T'
+        elif matrix[l][c] == 'F':
+            matrix[l][c] = 't'
+        elif matrix[l][c] == 'f':
+            matrix[l][c] = 'F'
 
 #######################
 # Write modified file #
